ac3cfd88b86c4c6c740ce56d6275bc0e407bb313
Gonzalez32 <3232rose@gmail.com>
2021-07-31 11:56:25 -0500

    modified urls/views and create note using django REST framework

--- backend_note/api/views.py
+++ backend_note/api/views.py
@@ -57,3 +57,14 @@ def getNote(request, pk):
     note = Note.objects.get(id=pk)
     serializer = NoteSerializer(note, many=False)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def createNote(request):
+    data = request.data
+
+    note = Note.objects.create(
+        body=data['body']
+    )
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
